fa1a80ca33cbca40c3fa56bcc730c3ffc12061ce
LeeSkies <lee2dskies@gmail.com>
2024-09-03 13:59:34 +0300

    Initial commit

---
+++ android/src/main/java/com/leeskies/capacitor/usbserialplugin/UsbSerial.java
@@ -0,0 +1,11 @@
+package com.leeskies.capacitor.usbserialplugin;
+
+import android.util.Log;
+
+public class UsbSerial {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
