24c1012ff170a2d8a0c7f1028aea0a191392c162
Mathieu Naon <naon.mathieu@gmail.com>
2025-08-07 06:58:32 +0200

    Include built dist with type declarations

---
+++ dist/plugin.cjs.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var core = require('@capacitor/core');
+
+const UsbSerialPrimitive = core.registerPlugin('UsbSerial');
+const getDeviceHandlers = async () => {
+    const deviceConnections = await UsbSerialPrimitive.getDeviceConnections();
+    const deviceHandlers = deviceConnections.devices.map(device => ({
+        device,
+        async connect(options) {
+            await UsbSerialPrimitive.openConnection(Object.assign({ deviceId: this.device.deviceId }, options));
+        },
+        async disconnect() {
+            await UsbSerialPrimitive.endConnection({ key: this.device.deviceKey });
+        },
+        async write(message) {
+            const response = await UsbSerialPrimitive.write({ key: this.device.deviceKey, message });
+            return response;
+        },
+        async read() {
+            return await UsbSerialPrimitive.read({ key: this.device.deviceKey });
+        },
+    }));
+    return deviceHandlers;
+};
+
+exports.UsbSerial = UsbSerialPrimitive;
+exports.getDeviceHandlers = getDeviceHandlers;
+//# sourceMappingURL=plugin.cjs.js.map
